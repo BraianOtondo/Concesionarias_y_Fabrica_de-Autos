ea2495b257a9fcd5ea66479f10e8fbb3538d7a52
BraianOtondo <braian_otondo99@outlook.com>
2021-10-17 17:31:40 -0300

    Linea de montaje en Automovil agregada

--- STP/concecionariaABM.sql
+++ STP/concecionariaABM.sql
@@ -1,35 +1,39 @@
 USE automotriz ;
 /*ALTA*/
 DELIMITER $$
-CREATE PROCEDURE agregarConcesionaria ( in _cuit_concesionaria int, in nombre varchar(45), out mensaje varchar(30))
+CREATE PROCEDURE agregarConcesionaria ( in _cuit_concesionaria int, in nombre varchar(45), out mensaje varchar(30),out resultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
     select count(*) into cantidadRepetida from concesionaria where cuit_concesionaria = _cuit_concesionaria;
     
     if (cantidadRepetida > 0 ) then
         select "Esta Concesionariaya existe" into mensaje;
+        select 0 into resultado;
 	else
 		insert into concesionaria values(_cuit_concesionaria,nombre);
 		select "Se agregó correctamente" into mensaje;
+        select 1 into resultado;
     end if;
 
 END$$
 DELIMITER ;
 /*BAJA*/
 DELIMITER $$
-CREATE PROCEDURE eliminarConcecionaria( in _cuit_concesionaria int ,out mensaje varchar(100))
+CREATE PROCEDURE eliminarConcecionaria( in _cuit_concesionaria int ,out mensaje varchar(100),out resultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
     declare cantidadPedido int default 0;
     select count(*) into cantidadRepetida from concesionaria where cuit_concesionaria=_cuit_concesionaria;
     select count(*) into cantidadPedido from pedido where concesionaria_cuit_concesionaria= _cuit_concesionaria;
-    
+	select 0 into resultado;
     if(cantidadRepetida > 0) then
 		if(cantidadPedido > 0) then
             select "No Se Puede eliminar la concensionaria debido a que tiene uno o mas pedidos" into mensaje;
 		else
             delete from concesionaria where cuit_concesionaria = _cuit_concesionaria;
             select"La concecionaria se eliminó correctamente" into mensaje;
+                    select 1 into resultado;
+
 		end if;
 	else
         select "La Concensionaria que quiere eliminar no existe" into mensaje;
@@ -38,18 +42,20 @@ END$$
 DELIMITER ;
 /*MODIFICACION*/
 DELIMITER $$
-CREATE PROCEDURE modificarConcensionaria( in _cuit_concesionaria int, in nuevo_cuit_concesionaria int, in nuevo_nombre varchar(45), out mensaje varchar(50))
+CREATE PROCEDURE modificarConcensionaria( in _cuit_concesionaria int, in nuevo_cuit_concesionaria int, in nuevo_nombre varchar(45), out mensaje varchar(50),out resultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
     select count(*) into cantidadRepetida from concesionaria where cuit_concesionaria =_cuit_concesionaria;
-    
+    select 0 into resultado;
+
     if(cantidadRepetida > 0) then
 		if(nuevo_cuit_concesionaria <> (select cuit_concesionaria from concesionaria where cuit_concesionaria=_cuit_concesionaria) 
 			and nuevo_nombre <> (select nombre from concesionaria where cuit_concesionaria=_cuit_concesionaria) ) then
             
 			UPDATE concesionaria set cuit_concesionaria= nuevo_cuit_concesionaria, nombre = nuevo_nombre where cuit_concesionaria= _cuit_concesionaria;
             select "La concesionaria se acutalizó correctamente" into mensaje;
-		
+            select 1 into resultado;
+
         else 
             select "Error al Modificar debido a que esta ingresando uno o mas datos repetidos" into mensaje;
 		end if;
@@ -60,14 +66,17 @@ BEGIN
 END$$
 DELIMITER ;
 select *from concesionaria;
-call agregarConcesionaria(1,"TodoAutos Temperley",@mensaje);
+call agregarConcesionaria(1,"TodoAutos Temperley",@mensaje,@resultado);
 select @mensaje;
+select @resultado;
 call agregarConcesionaria(3,"TODOAUTOS MONTE",@mensaje);
 select @mensaje;
-call eliminarConcecionaria(1,@mensaje);
+call eliminarConcecionaria(1,@mensaje,@resultado);
 select @mensaje;
-call modificarConcensionaria(1,4,"TodoAutos Luis Guillon",@mensaje);
+select @resultado;
+call modificarConcensionaria(1,4,"TodoAutos Luis Guillon",@mensaje,@resultado);
 select @mensaje;
+select @resultado;
 -- (in _id_pedido int,in _id_modelo int,in _cuit_concesionaria int,in _cantidad int,out mensaje varchar(50))
 insert into modelo values(27,"FORD RANGER");
 insert into modelo values(77,"KANGOO");
--- STP/pedidoABM.sql
+++ STP/pedidoABM.sql
@@ -1,11 +1,14 @@
 USE automotriz ;
 /*ALTA*/
 DELIMITER $$
-create procedure agregarPedido(in _id_pedido int,in _id_modelo int,in _cuit_concesionaria int,in _cantidad int,out mensaje varchar(70)) begin
+create procedure agregarPedido(in _id_pedido int,in _id_modelo int,in _cuit_concesionaria int,in _cantidad int,out mensaje varchar(70),out resultado int) begin
 declare cantidadRepetida int default 0;
 select count(*) into cantidadRepetida from pedido where id_pedido=_id_pedido;
+select 0 into resultado;
 if(cantidadRepetida>0)then
 	select "Se agrego el detalle_pedido en el pedido correctamente" into mensaje; -- si ya existe el pedido solo agrega
+    select 1 into resultado;
+
     insert into detalle_pedido values(_id_modelo,_id_pedido,_cantidad);
 else
 	select "Se creó el pedido y se agregó el detalle_pedido correctamente" into mensaje; -- si no existe, lo crea y luego lo agrega
@@ -16,15 +19,16 @@ end$$
 DELIMITER ;
 /*BAJA*/
 DELIMITER $$
-CREATE PROCEDURE eliminarPedido(in _id_pedido int, out mensaje varchar(70))
+CREATE PROCEDURE eliminarPedido(in _id_pedido int, out mensaje varchar(70),out resultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
     select count(*) into cantidadRepetida from detalle_pedido where  pedido_id_pedido = _id_pedido;
-															
+	select 0 into resultado;												
     if(cantidadRepetida > 0) then
         delete from detalle_pedido where pedido_id_pedido = _id_pedido;
         delete from pedido where id_pedido =_id_pedido;
 		select "El pedido se eliminó correctamente" into mensaje;
+		select 1 into resultado;
     else
         select "El pedido que quiere eliminar no existe" into mensaje;
 	end if;
@@ -32,11 +36,11 @@ END$$
 DELIMITER ;
 /*MODIFICACION*/
 DELIMITER $$
-CREATE PROCEDURE modificarPedido(in _id_pedido int,in _id_modelo int, in nueva_cantidad int, in nuevo_id_modelo int, out mensaje varchar(70))
+CREATE PROCEDURE modificarPedido(in _id_pedido int,in _id_modelo int, in nueva_cantidad int, in nuevo_id_modelo int, out mensaje varchar(70),out resultado int)
 BEGIN
 	declare cantidadRepetida int default 0;
-		select count(*) into cantidadRepetida from detalle_pedido where  pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo;
-        
+	select count(*) into cantidadRepetida from detalle_pedido where  pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo;
+	select 0 into resultado;												    
     if(cantidadRepetida > 0) then
 		if(nueva_cantidad <> (select cantidad from detalle_pedido where  pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo)) 
 			and (nuevo_id_modelo <> (select modelo_id_modelo from detalle_pedido where  pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo)) then
@@ -44,7 +48,7 @@ BEGIN
             update detalle_pedido set cantidad = nueva_cantidad where pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo;
             update detalle_pedido set modelo_id_modelo=nuevo_id_modelo where pedido_id_pedido = _id_pedido and modelo_id_modelo=_id_modelo;
 			select "El pedido se actulizo correctamente" into mensaje;
-
+			select 1 into resultado;												
         else
             select "No Se Puede Modificar debido a que estan repitiendo uno o mas datos" into mensaje;
 		end if;
